c250c8b167e1cc4f3641ec5e441a9fc808f72dbe
Albert Huynh <alberthuynh91@gmail.com>
2023-03-10 17:03:34 -0800

    Add comment

--- utils/custom-image-loader.ts
+++ utils/custom-image-loader.ts
@@ -1,3 +1,4 @@
+// Manipulate image src to use correct size and image format based on screen size and device
 export const customLoader = (props): string => {
   const { src, width } = props;
   const newSrc = src.split('/');
